0051b5651997008d2ee4c7431b8e6efbd886488b
Laura Gil <laura.gilcara@gmail.com>
2024-10-22 22:00:10 +0200

    merge main with dev

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,35 @@ import Footer from "./components/Footer";
 import ScrollButton from "./components/heroSection/ScrollButton";
 import FormSection from "./components/formSection/FormSection";
 function App() {
-  const sectionsRef = useRef({
-    popular: null,
-    form: null,
-  });
+    const sectionsRef = useRef({
+        popular: null,
+        form: null,
+    });
 
-  const handleScroll = (key) => {
-    if (sectionsRef.current[key]) {
-      sectionsRef.current[key].scrollIntoView({ behavior: "smooth" });
-    }
-  };
+    const handleScroll = (key) => {
+        if (sectionsRef.current[key]) {
+            sectionsRef.current[key].scrollIntoView({ behavior: "smooth" });
+        }
+    };
 
-  return (
-    <>
-      <Navbar handleScroll={handleScroll} />
-      <main className="bg-mediumBlue text-white font-sora">
-        <HeroSection />
-        <ScrollButton
-          handleScroll={handleScroll}
-          target="popular"
-          ref={(el) => (sectionsRef.current.popular = el)}
-        ></ScrollButton>
-        <PopularSection ref={(el) => (sectionsRef.current.popular = el)} />
-        <FormSection ref={(el) => (sectionsRef.current.form = el)} />
-      </main>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar handleScroll={handleScroll} />
+            <main className="bg-mediumBlue text-white font-sora">
+                <HeroSection />
+                <ScrollButton
+                    handleScroll={handleScroll}
+                    target="popular"
+                    ref={(el) => (sectionsRef.current.popular = el)}
+                ></ScrollButton>
+                <PopularSection
+                    ref={(el) => (sectionsRef.current.popular = el)}
+                />
+                <FormSection ref={(el) => (sectionsRef.current.form = el)} />
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
