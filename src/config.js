58195bc915db5974ffcca02d82ab76ec87409933
Laura Gil <laura.gilcara@gmail.com>
2024-10-23 00:08:40 +0200

    add fetch to neighborhoodsAndActivities endpoint

---
+++ src/config.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = "http://localhost:9000/barrisVius"
+
+export const GET_DISTRICTS_ACTIVITIES = `${API_BASE_URL}/neighborhoodsAndActivities`
--- src/customHooks/useFetch.jsx
+++ src/customHooks/useFetch.jsx
@@ -7,7 +7,12 @@ const useFetch = (url) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(url);
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers: {
+                        "X-Requested-With": "XMLHttpRequest",
+                    },
+                });
                 if (!response.ok) {
                     throw new Error(
                         `Error fetching data: ${response.statusText}`
